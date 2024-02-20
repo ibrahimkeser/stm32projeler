a351f6ac32926c694a4a3be80961e856195ab1e8
İbrahim Keser <ibrahimkeser470@gmail.com>
2024-02-20 22:17:24 +0300

    Stm32 projeler
    stm32 nucleo-64 F302R8 geliştirme kartı ile 7 segment display uygulaması

---
+++ 7 segment display/Core/Inc/display.h
@@ -0,0 +1,15 @@
+/*
+ * display.h
+ *
+ *  Created on: Oct 14, 2023
+ *      Author: ibrah
+ */
+
+#ifndef INC_DISPLAY_H_
+#define INC_DISPLAY_H_
+
+#include "stm32f3xx_hal.h"
+void display(int a);
+
+#endif /* INC_DISPLAY_H_ */
+
